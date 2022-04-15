244229fc3f6a7c7b77d4e56526005f00991e529d
Sajees XPS <sajees.jeya@gmail.com>
2022-04-15 17:42:01 -0400

    forms

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
@@ -14,9 +15,14 @@ const Home = () => {
   const loggedIn = Auth.loggedIn();
 
   return (
-<main>
-  <div className="flex-row justify-space-between">
-    <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+    <main>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -25,7 +31,7 @@ const Home = () => {
               title="Some Feed for Thought(s)..."
             />
           )}
-      </div>
+        </div>
       {loggedIn && userData ? (
           <div className="col-12 col-lg-3 mb-3">
             <FriendList
